df3526af9c4c44c4ccc865e3594131d33a89d712
Your Name <you@example.com>
2025-09-21 20:50:10 +0800

    this is the first commit

---
+++ ice_cream_query.py
@@ -0,0 +1,9 @@
+
+want_ice_cream = input("Do you want ice cream? (yes/no): ")
+
+if want_ice_cream.lower() == "yes":
+    print("Great! Enjoy your ice cream!")
+elif want_ice_cream.lower() == "no":
+    print("No problem. Maybe next time!")
+else:
+    print("I'm sorry, I didn't understand your response. Please answer with 'yes' or 'no'.")
